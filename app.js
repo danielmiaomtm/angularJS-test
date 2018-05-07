d9243c6be400bd6eea566339d5a2a581db1557e2
Daniel Miao <jeffreymaomtm@gmail.com>
2018-05-07 13:03:08 -0700

    mod1_solution

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-var student = {
-	name: '',
-	type: 'student'
-};
+(function () {
+'use strict';
 
-document.addEventListener('DOMContentLoaded', contentLoaded);
+angular.module('lunchChecker', [])
 
-function contentLoaded (event) {
-	document.getElementById('name').addEventListener('keyup', keyUp);
-}
+.controller('lunchCheckerController', function ($scope) {
+  $scope.lunchMenu = '';
+  $scope.warningLabel = {
+  	label: '',
+  	color: ''
+  };
 
-function keyUp (event) {
-	calculateNumericOutput();		
-}
+  $scope.checkLunchMenu = function () {
+    $scope.warningLabel = getNumberOfItems($scope.lunchMenu);
+  };
 
-function calculateNumbericOutput () {
-	student.name = document.getElementById('name').value;
-	var totalNameValue = 0;
-	for (var i = 0; i < student.name.length; i++) {
-		totalNameValue += student.name.charCodeAt(i);
-	}
 
-	// Insert result into page
-	var output = "Total Numeric value of person's name is " + totalNameValue;
-	document.getElementById('output').innerText = output;
-}
+  function getNumberOfItems(string) {
+  	let states = [{
+			label: 'Please enter data first',
+  		color: 'red'
+  	}, {
+  		label: 'Enjoy!',
+  		color: 'green'
+  	}, {
+  		label: 'Too much!',
+  		color: 'green'
+  	}];
+
+  	if (string === null || string.length === 0) {
+  		return states[0];
+  	}
+    let count = getNonEmptyItems(string);
+   	return count === 0 ? states[0] : (count <= 3 ? states[1] : states[2]);
+  }
+
+  function getNonEmptyItems (string) {
+  	let items = string.split(',');
+  	var count = 0;
+    items.forEach(function (item) {
+			if (item.trim().length !== 0) {
+    		count++;
+    	}
+    });
+    return count;
+  }
+
+});
+
+
+})();
